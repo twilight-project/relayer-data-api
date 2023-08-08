1c0c32c0bd4393d791b166f478811a847a8e591f
TJ <tjsharp@vultr.guest>
2023-08-08 10:30:38 +0000

    Add index to timestamps

---
+++ migrations/2023-08-08-101917_add-ts-index/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX timestamp_btc_usd_price;
+DROP INDEX timestamp_funding_rate;
+DROP INDEX timestamp_trader_order;
+DROP INDEX timestamp_lend_order;
