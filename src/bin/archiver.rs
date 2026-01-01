91a66616ef2d692d2a76dafb205aafe883bc946b
Siddharth Parashar <20308880+shanu516516@users.noreply.github.com>
2026-01-01 11:50:25 +0000

    Update environment variable usage in WebSocket and Archiver modules
    - Added new environment variables for Kafka group configurations in `.env.example`.
    - Refactored `ws.rs` and `archiver.rs` to utilize these environment variables instead of hardcoded values for `SNAPSHOT_TOPIC` and `WEBSOCKET_GROUP`.
    - Improved flexibility and configurability of Kafka consumer initialization.

--- src/bin/archiver.rs
+++ src/bin/archiver.rs
@@ -3,8 +3,8 @@ use log::warn;
 use relayerarchiverlib::kafka;
 use relayerarchiverlib::DatabaseArchiver;
 
-const SNAPSHOT_TOPIC: &str = "CoreEventLogTopic";
-const ARCHIVER_GROUP: &str = "Archiver_Redis";
+// const SNAPSHOT_TOPIC: &str = "CoreEventLogTopic";
+// const ARCHIVER_GROUP: &str = "Archiver_Redis";
 
 fn main() {
     tracing_subscriber::fmt::Subscriber::builder()
@@ -20,9 +20,11 @@ fn main() {
     let database_url = std::env::var("DATABASE_URL").expect("No database url found!");
     let redis_url = std::env::var("ORDERBOOK_REDIS").expect("No redis url found!");
 
+    let snapshot_topic = std::env::var("CORE_EVENT_LOG").unwrap_or("CoreEventLogTopic".to_string());
+    let archiver_group =
+        std::env::var("ARCHIVER_KAFKA_GROUP").unwrap_or("Archiver_Redis".to_string());
     let (tx, rx) = unbounded();
-    let (completions, _handle) =
-        kafka::start_consumer(ARCHIVER_GROUP.into(), SNAPSHOT_TOPIC.into(), tx);
+    let (completions, _handle) = kafka::start_consumer(archiver_group, snapshot_topic, tx);
 
     let database_worker = DatabaseArchiver::from_host(&database_url, &redis_url, completions);
 
